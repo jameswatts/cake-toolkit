767b60f183b492c6f66d8de9d61b811db719bf6d
James Watts <james.watts@cakedc.com>
2013-02-17 16:26:18 +0100

    Updated CtkBuildable with additional features for traversing the object model and manipulating objects

--- Lib/CtkBuildable.php
+++ Lib/CtkBuildable.php
@@ -25,7 +25,16 @@
 interface CtkBuildable {
 
 /**
+ * Determines if the node has a parent node.
+ *
+ * @return boolean
+ */
+	public function hasParent();
+
+/**
  * Returns the parent node of this node.
+ *
+ * @return CtkBuildable
  */
 	public function getParent();
 
@@ -33,40 +42,131 @@ interface CtkBuildable {
  * Sets the parent node for this node.
  *
  * @param CtkBuildable $node The parent node.
+ * @return CtkBuildable
  */
 	public function setParent(CtkBuildable $node = null);
 
 /**
  * Determines if the node has child nodes.
+ *
+ * @return boolean
  */
 	public function hasChildren();
 
 /**
+ * Determines if a node is a child of this node.
+ *
+ * @return boolean
+ */
+	public function hasChild(CtkBuildable $node);
+
+/**
  * Returns the child nodes of this node.
+ *
+ * @return array
  */
 	public function getChildren();
 
 /**
  * Returns the first child node of this node.
+ *
+ * @return CtkBuildable
  */
 	public function getFirst();
 
 /**
  * Returns the last child node of this node.
+ *
+ * @return CtkBuildable
  */
 	public function getLast();
+
+/**
+ * Returns the previous node before this node in the common parent.
+ *
+ * @return CtkBuildable
+ */
+	public function getPrevious();
+
+/**
+ * Returns the next node after this node in the common parent.
+ *
+ * @return CtkBuildable
+ */
+	public function getNext();
 
 /**
  * Adds a node to this node as a child.
  *
  * @param CtkBuildable $node Child node.
+ * @return CtkBuildable
  */
 	public function add(CtkBuildable $node);
+
+/**
+ * Adds a node before the specified node.
+ *
+ * @param CtkBuildable $node Child node.
+ * @param CtkBuildable $before Node to add before.
+ * @return CtkBuildable
+ */
+	public function addBefore(CtkBuildable $node, CtkBuildable $before);
+
+/**
+ * Adds a node after the specified node.
+ *
+ * @param CtkBuildable $node Child node.
+ * @param CtkBuildable $after Node to add after.
+ * @return CtkBuildable
+ */
+	public function addAfter(CtkBuildable $node, CtkBuildable $after);
+
+/**
+ * Adds an array of nodes to this node as children.
+ *
+ * @param array $nodes The child nodes.
+ * @return CtkBuildable
+ */
+	public function addMany(array $nodes);
+
+/**
+ * Inherits the children of another node.
+ *
+ * @param CtkBuildable $node The node to inherit from.
+ * @param boolean $prepend Nodes should be added before existing children.
+ * @return CtkBuildable
+ */
+	public function addFrom(CtkBuildable $node, $prepend = false);
+
+/**
+ * Replaces the specified node with the given node.
+ *
+ * @param CtkBuildable $node Child node.
+ * @param CtkBuildable $replace Node to replace.
+ * @return CtkBuildable
+ */
+	public function replaceChild(CtkBuildable $node, CtkBuildable $replace);
+
+/**
+ * Removes and returns a child node from this node.
+ *
+ * @param CtkBuildable $node Child node.
+ * @return CtkBuildable
+ */
+	public function removeChild(CtkBuildable $node);
+
+/**
+ * Removes all children from this node.
+ *
+ * @return CtkBuildable
+ */
+	public function clearChildren();
 
 /**
  * Determines if any event types or a specific event type has been set.
  *
  * @param string $type The event type.
+ * @return boolean
  */
 	public function hasEvents($type = null);
 
@@ -74,6 +174,7 @@ interface CtkBuildable {
  * Returns the events for a specific event type.
  *
  * @param string $type The event type.
+ * @return array
  */
 	public function getEvents($type);
 
@@ -82,6 +183,7 @@ interface CtkBuildable {
  *
  * @param string $type The event type.
  * @param CtkEvent $event The event object.
+ * @return CtkBuildable
  */
 	public function addEvent($type, CtkEvent $event);
 
@@ -89,6 +191,7 @@ interface CtkBuildable {
  * Removes a previously set event from the element.
  *
  * @param string $type The event type.
+ * @return CtkBuildable
  */
 	public function removeEvents($type);
 
@@ -96,6 +199,7 @@ interface CtkBuildable {
  * Removes all events previously set on the element.
  *
  * @param string $type The event type.
+ * @return CtkBuildable
  */
 	public function clearEvents($type = null);
 }
