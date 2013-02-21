e8f442e7783cf34e70965a2a2fd3d9d474a3175f
James Watts <james.watts@cakedc.com>
2013-02-21 05:32:34 +0100

    Added getSingleton() and _inheritArrayProperties() utility methods

--- Lib/CtkObject.php
+++ Lib/CtkObject.php
@@ -25,18 +25,80 @@
 abstract class CtkObject extends Object {
 
 /**
+ * The collection of singleton instances.
+ *
+ * @var array
+ */
+	protected static $_instanceCollection = array();
+
+/**
+ * Gets a singleton of the current class.
+ *
+ * @param array $arguments Optional arguments for the class constructor.
+ * @return CtkObject
+ */
+	final public static function getSingleton(array $arguments = array()) {
+		$name = get_called_class();
+		if (isset(self::$_instanceCollection[$name])) {
+			return self::$_instanceCollection[$name];
+		}
+		$class = new ReflectionClass($name);
+		self::$_instanceCollection[$name] = $class->newInstanceArgs($arguments);
+		return self::$_instanceCollection[$name];
+	}
+
+/**
  * Gets an instance of the current class.
  *
  * @param array $arguments Optional arguments for the class constructor.
  * @return CtkObject
  */
-	final public static function getInstance(array $arguments = null) {
-		if (is_array($arguments)) {
+	final public static function getInstance(array $arguments = array()) {
+		if (sizeof($arguments)) {
 			$class = new ReflectionClass(get_called_class());
 			return $class->newInstanceArgs($arguments);
 		} else {
 			return new static();
 		}
+	}
+
+/**
+ * Inherits the values of common array properties in the class inheritence chain.
+ *
+ * @param array $properties The array properties to inherit.
+ * @param boolean $typeSafe Optional request that inherited values are of the same type.
+ * @return CtkObject
+ */
+	final protected function _inheritArrayProperties(array $properties = array(), $typeSafe = false) {
+		if (sizeof($properties)) {
+			$class = new ReflectionClass(get_called_class());
+			$chain = array();
+			while (($class = $class->getParentClass()) !== false) {
+				if ($class->getName() === __CLASS__) {
+					break;
+				}
+				$chain[] = $class;
+			}
+			array_reverse($chain);
+			foreach ($chain as $i => $class) {
+				$defaultProperties = $class->getDefaultProperties();
+				foreach ($properties as $j => $property) {
+					if (is_array($this->$property) && isset($defaultProperties[$property]) && is_array($defaultProperties[$property])) {
+						foreach ($defaultProperties[$property] as $key => $value) {
+							if (!isset($this->{$property}[$key])) {
+								$this->{$property}[$key] = $value;
+							}
+						}
+						for ($k = 0; $k < count($defaultProperties[$property]); $k++) {
+							if (isset($defaultProperties[$property][$k]) && !in_array($defaultProperties[$property][$k], $this->$property)) {
+								$this->{$property}[] = $defaultProperties[$property][$k];
+							}
+						}
+					}
+				}
+			}
+		}
+		return $this;
 	}
 }
 
