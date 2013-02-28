de3502e94ceb6921d3f6fa7a191e4dcd6d0816df
James Watts <james.watts@cakedc.com>
2013-02-28 03:44:20 +0100

    Added the ability to define sub-factories and helpers in the factory class

--- Lib/CtkFactory.php
+++ Lib/CtkFactory.php
@@ -18,8 +18,10 @@
  */
 
 App::uses('Set', 'Utility');
+App::uses('HelperCollection', 'View');
 App::uses('CtkObject', 'Ctk.Lib');
 App::uses('CtkView', 'Ctk.View');
+App::uses('CtkHelper', 'Ctk.View');
 
 /**
  * Abstract class for defining view object factories.
@@ -34,6 +36,20 @@ abstract class CtkFactory extends CtkObject {
  * @var array
  */
 	public $settings = array();
+
+/**
+ * An array of names of element factories to include.
+ *
+ * @var mixed A single name as a string or a list of names as an array.
+ */
+	public $factories = array();
+
+/**
+ * An array containing the names of helpers this factory uses.
+ *
+ * @var mixed A single name as a string or a list of names as an array.
+ */
+	public $helpers = array();
 
 /**
  * The current view calling the factory.
@@ -57,6 +73,20 @@ abstract class CtkFactory extends CtkObject {
 	protected $_plugin = null;
 
 /**
+ * Sub-factories defined to use in this factory.
+ *
+ * @var array
+ */
+	protected $_factories = array();
+
+/**
+ * The helpers available.
+ *
+ * @var array
+ */
+	protected $_helpers = array();
+
+/**
  * Contructor
  *
  * Creates a new factory with a reference to the current view.
@@ -67,11 +97,64 @@ abstract class CtkFactory extends CtkObject {
  * @param array $settings Configuration settings for the factory.
  */
 	final public function __construct(CtkView $view, $name, $plugin = null, $settings = null) {
+		parent::__construct();
 		$this->_view = $view;
 		$this->_name = (string) $name;
 		$this->_plugin = (string) $plugin;
 		if ($settings) {
 			$this->settings = Set::merge($this->settings, (array) $settings);
+		}
+		$this->_inheritArrayProperties(array('factories', 'helpers'));
+		$this->_factories = (empty($this->factories))? array() : Set::normalize((array) $this->factories);
+		foreach ($this->_factories as $key => $value) {
+			$isAlias = (is_array($value) && isset($value['className']));
+			list($plugin, $name) = pluginSplit(($isAlias)? $value['className'] : $key);
+			$class = $name . 'Factory';
+			App::uses($class, ((!empty($plugin))? $plugin . '.' : '') . 'View/Factory');
+			if (!class_exists($class)) {
+				throw new CakeException(sprintf('Unknown factory: %s', $class));
+			}
+			$property = ($isAlias)? $key : $name;
+			$this->$property = new $class($this->_view, $name, $plugin, $value);
+			$this->$property->setup();
+		}
+		$helpers = HelperCollection::normalizeObjectArray((empty($this->helpers))? array() : Set::normalize((array) $this->helpers));
+		$helperCollection = new HelperCollection($this->_view->getBaseView());
+		foreach ($helpers as $name => $properties) {
+			list($plugin, $class) = pluginSplit($properties['class']);
+			$this->_helpers[$class] = new CtkHelper($class, $helperCollection->load($properties['class'], $properties['settings']), $this->_view);
+		}
+	}
+
+/**
+ * Returns a helper or setting from the factory.
+ *
+ * @param string $name Name of helper or setting.
+ * @return mixed
+ * @throws CakeException if the helper or setting is not found.
+ */
+	final public function __get($name) {
+		if (isset($this->_helpers[(string) $name])) {
+			return $this->_helpers[(string) $name];
+		}
+		if (isset($this->settings[(string) $name])) {
+			return $this->settings[(string) $name];
+		}
+		throw new CakeException(sprintf('Unknown helper or setting: %s', $name));
+	}
+
+/**
+ * Adds a sub-factory to this factory.
+ *
+ * @param string $name Name of the factory.
+ * @param CtkFactory $factory The factory object.
+ * @throws CakeException if factory has not been previously included.
+ */
+	final public function __set($name, $factory) {
+		if (is_object($factory) && $factory instanceof CtkFactory) {
+			$this->$name = $factory;
+		} else {
+			throw new CakeException(sprintf('Invalid factory: %s', $name));
 		}
 	}
 
@@ -90,6 +173,24 @@ abstract class CtkFactory extends CtkObject {
 			throw new CakeException(sprintf('Unknown object: %s', $class));
 		}
 		return new $class($this, (count($arguments) > 0)? (array) $arguments[0] : array());
+	}
+
+/**
+ * Returns the sub-factories used by this factory.
+ *
+ * @return array
+ */
+	final public function getFactories() {
+		return $this->_factories;
+	}
+
+/**
+ * Returns the helpers used by this factory.
+ *
+ * @return array
+ */
+	final public function getHelpers() {
+		return $this->_helpers;
 	}
 
 /**
